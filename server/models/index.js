84a46728f04ce15374cadfdd35e99d6807b2387a
judy-her <judith85dm@ucla.edu>
2024-04-15 14:29:13 -0700

    added seeds file in config folder with seeds for Category, Exercise, and User

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Exercise = require('./Exercise');
 const Category = require('./Category');
+const Order = require('./Order');
 
-module.exports = { User, Exercise, Category };
+module.exports = { User, Exercise, Category, Order };
