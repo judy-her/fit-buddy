1e562509d8f96941558ffe0633c8b21c8c1e9fc1
judy-her <judith85dm@ucla.edu>
2024-04-15 14:47:38 -0700

    edited and added all types

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,25 +1,25 @@
-const Exercise = require('../models/Exercise');
+// const { User, Exercise, Category } = require('../models');
 
-const resolvers = {
-  Query: {
-    exercises: async () => {
-      return Exercise.find();
-    },
+// const resolvers = {
+//   Query: {
+//     exercises: async () => {
+//       return Exercise.find();
+//     },
 
-    exercise: async (parent, { exerciseId }) => {
-      return Exercise.findOne({ _id: exerciseId });
-    },
-  },
+//     exercise: async (parent, { exerciseId }) => {
+//       return Exercise.findOne({ _id: exerciseId });
+//     },
+//   },
 
-  Mutation: {
-    addExercise: async (parent, { exerciseTitle, exerciseAuthor }) => {
-      return Exercise.create({ exerciseTitle, exerciseAuthor });
-    },
+//   Mutation: {
+//     addExercise: async (parent, { exerciseTitle, exerciseAuthor }) => {
+//       return Exercise.create({ exerciseTitle, exerciseAuthor });
+//     },
 
-    removeExercise: async (parent, { exerciseId }) => {
-      return Exercise.findOneAndDelete({ _id: exerciseId });
-    },
-  },
-};
+//     removeExercise: async (parent, { exerciseId }) => {
+//       return Exercise.findOneAndDelete({ _id: exerciseId });
+//     },
+//   },
+// };
 
-module.exports = resolvers;
+// module.exports = resolvers;
