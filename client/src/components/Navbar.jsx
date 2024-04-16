400877b46be7b11533121ed5621d0b42d2094b59
judy-her <judith85dm@ucla.edu>
2024-04-16 13:47:14 -0700

    added Navbar comp to app.jsx, changed navbar styling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,46 +9,52 @@ function Navbar() {
   };
 
   return (
-    <nav className="navbar">
-      <ul className="nav-links">
-        <li>
-          <Link
-            to="/dashboard"
-            className={
-              currentPage === '/dashboard' ? 'nav-link active' : 'nav-link'
-            }
-            onClick={() => handleNavigationClick('dashboard')}
-          >
-            Dashboard
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/excercises"
-            className={
-              currentPage === '/excercises' ? 'nav-link active' : 'nav-link'
-            }
-            onClick={() => handleNavigationClick('excercises')}
-          >
-            Excercises
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/explore"
-            className={
-              currentPage === '/explore' ? 'nav-link active' : 'nav-link'
-            }
-            onClick={() => handleNavigationClick('explore')}
-          >
-            Explore
-          </Link>
-        </li>
-        <li>
-          {/* <Link to="/logout">Log Out</Link> */}
-          <button> Log Out </button>
-        </li>
-      </ul>
+    <nav className="navbar header gradient-background navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="logo text-gradient" href="#">
+          FitBuddy
+        </a>
+
+        <ul className="nav-links">
+          <li>
+            <Link
+              to="/dashboard"
+              className={
+                currentPage === '/dashboard' ? 'nav-link active' : 'nav-link'
+              }
+              onClick={() => handleNavigationClick('dashboard')}
+            >
+              Dashboard
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/excercises"
+              className={
+                currentPage === '/excercises' ? 'nav-link active' : 'nav-link'
+              }
+              onClick={() => handleNavigationClick('excercises')}
+            >
+              Excercises
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/explore"
+              className={
+                currentPage === '/explore' ? 'nav-link active' : 'nav-link'
+              }
+              onClick={() => handleNavigationClick('explore')}
+            >
+              Explore
+            </Link>
+          </li>
+          <li>
+            {/* <Link to="/logout">Log Out</Link> */}
+            <button> Log Out </button>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
